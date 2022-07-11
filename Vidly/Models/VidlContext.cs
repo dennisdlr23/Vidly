634cf4bd04b430573ef2bea1b3b9bd846457a542
dennisdlr23 <dennisdelarosa250@gmail.com>
2022-07-11 02:37:44 -0600

    Add project files.

---
+++ Vidly/Models/VidlContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class VidlContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+    }
+}
